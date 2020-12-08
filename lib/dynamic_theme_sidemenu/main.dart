371a49636abc4eb58a3a8394300244a388da4abd
GeonwooVincentKim <kdsnop@gmail.com>
2020-12-08 16:03:16 +0900

    Testing..

--- lib/dynamic_theme_sidemenu/main.dart
+++ lib/dynamic_theme_sidemenu/main.dart
@@ -1,6 +1,10 @@
 import 'dart:async';
 
+import 'package:dynamic_theme/dynamic_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app_test/dynamic_theme/body.dart';
+import 'package:flutter_app_test/dynamic_theme/test.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,6 +12,36 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // return MultiProvider(
+    //   providers: [
+
+    //   ],
+    //   child: DynamicTheme(
+    //     defaultBrightness: Brightness.dark,
+    //     data: (brightness) => ThemeData(
+    //       primarySwatch: Colors.blue,
+    //       brightness: brightness
+    //     ),
+    //     themedWidgetBuilder: (context, snapshot) {
+    //       return MaterialApp(
+    //         theme: snapshot,
+    //         initialRoute: "/",
+    //         routes: {
+    //           "/": (context) => Body(title: "HI")
+    //         },
+    //         onGenerateRoute: (settings) {
+    //           final List<String> routeGenerator = settings.name.split("/");
+    //           if(routeGenerator[0] != '') return null;
+    //           if(routeGenerator[1] == 'test'){
+    //             String subRouteID = routeGenerator[2];
+    //             return MaterialPageRoute(builder: (BuildContext context) => Test(testID: subRouteID));
+    //           }
+    //         },
+    //         onUnknownRoute: (settings) => MaterialPageRoute(builder: (BuildContext context) => Body()),
+    //       );
+    //     },
+    //   )
+    // );
     return StreamBuilder(
       stream: bloc.darkThemeEnabled,
       initialData: false,
